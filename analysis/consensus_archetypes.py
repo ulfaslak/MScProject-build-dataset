8b10e1e3cbf67ac33a122efc4571bbace3883880
ulfaslak <ulfjensen@gmail.com>
2016-02-15 18:10:09 +0200

    played with consensus archetypes

--- analysis/consensus_archetypes.py
+++ analysis/consensus_archetypes.py
@@ -1,26 +1,42 @@
 import pandas as pd
 import numpy as np
 
-def get_weights(variance=False):
-    df_main = pd.read_csv('../data/archetypes.csv')
+df_main = pd.read_csv('data/archetypes.csv')
 
-    df1 = df_main.iloc[:,range(0,5)]
-    df2 = df_main.iloc[:,range(5,10)]
-    df3 = df_main.iloc[:,range(10,15)]
-    df4 = df_main.iloc[:,range(15,20)]
+df1 = df_main.iloc[:,range(0,5)]
+df2 = df_main.iloc[:,range(5,10)]
+df3 = df_main.iloc[:,range(10,15)]
+df4 = df_main.iloc[:,range(15,20)]
+dfs = [df1, df2, df3, df4]
 
-    # Find median, mean and std of each archetype trait
-    df_std = np.empty((6,5))
-    dfs = [df1, df2, df3, df4]
+def get_weights(use_variance=True):
+    W = np.empty((6,5))
 
     for i,_ in enumerate(df_main.iterrows()):
         for j in range(5):
             vals = [df.iloc[i,j] for df in dfs]
-            df_std[i,j] = np.std(vals)
-
-    df_std = pd.DataFrame(df_std, columns=['O','C','E','A','N'])
+            W[i,j] = np.std(vals)
     
-    if variance:
-        return 1/df_std**2
+    if use_variance:
+        return 1/W**2
+    if use_variance is False:
+        return 1/W
     else:
-        return 1/df_std
+        return W
+
+
+def get_archetypes(use_mean=False):
+    # Find median, mean and std of each archetype trait
+    A = np.empty((6,5))
+
+    for i,_ in enumerate(df_main.iterrows()):
+        for j in range(5):
+            vals = [df.iloc[i,j] for df in dfs]
+            if use_mean: A[i,j] = np.mean(vals)
+            else: A[i,j] = np.mean(vals)
+    
+    return A
+
+
+
+        
