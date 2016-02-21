18f267360ad12a9e86ecac91e48bd81adc31ae23
ulfaslak <ulfjensen@gmail.com>
2016-02-21 14:02:10 +0200

    added kd scheme to outlier_detection. probably has bugs

--- build_dataset/analysis/outlier_detection.py
+++ build_dataset/analysis/outlier_detection.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 from sklearn import svm
+from sklearn import neighbors
 from collections import defaultdict
 
-class Outlier_detector:
+class Outlier_detector_svm:
     """Compute outliers in X matrix
         Performs column-pair-wise topology estimation using one class SVM
         to estimate average topology score pr. datapoint and remove those
@@ -45,7 +46,7 @@ class Outlier_detector:
     
     
     def _compute_outliers(self):
-        self.pred_ys = defaultdict(list)
+        pred_ys = defaultdict(list)
 
         # Fit the problem for all vector combinations
         for i, combo in enumerate(self.component_combinations):
@@ -56,14 +57,14 @@ class Outlier_detector:
             y_pred = self.clf.decision_function(X).ravel()
 
             for j,_ in enumerate(y_pred):
-                self.pred_ys[j].append(y_pred[j])
+                pred_ys[j].append(y_pred[j])
 
         if self.hard:
-            self.pred_avgs = dict((k,np.min(v)) for k,v in self.pred_ys.items())
+            pred_avgs = dict((k,np.min(v)) for k,v in pred_ys.items())
         else:
-            self.pred_avgs = dict((k,np.mean(v)) for k,v in self.pred_ys.items())
+            pred_avgs = dict((k,np.mean(v)) for k,v in pred_ys.items())
 
-        outliers = [k for k,v in self.pred_avgs.items() if v < self.threshold]
+        outliers = [k for k,v in pred_avgs.items() if v < self.threshold]
     
         return outliers
     
@@ -127,11 +128,133 @@ class Outlier_detector:
 
         plt.show()
         
-
-        
         
     def main(self):
         outliers = self._compute_outliers()
         if self.visualize:
             self._show_svm_plots()
         return outliers
+
+
+class Outlier_detector_kd:
+    """Compute outliers in X matrix
+        Compute Kernel Density for each datapoint and mark outliers as
+        point below threshold density.
+        
+    Parameters
+    ----------
+    X : np.array
+        Data matrix of N, M dimensions.
+    threshold : float
+        Cut off value. Increase to remove more points. 
+    visualize : bool
+        Instruction whether to visualize detection scheme. Number of output
+        figures increases polynomialy with number of columns in X!
+        
+    Output
+    ------
+    outliers : list
+        List of indices for outlier points
+    """
+
+    def __init__(self, X, visualize=False, threshold=-0.5,  bandwidth=1.0, kernel='gaussian'):
+
+        self.X = X
+        self.visualize = visualize
+        self.threshold = threshold
+        self.N, self.M = X.shape
+
+        self.component_combinations = []
+        for i in range(self.M):
+            for j in range(self.M):
+                if j > i:
+                    self.component_combinations.append((i,j))
+                    
+        self.clf = neighbors.KernelDensity(bandwidth=bandwidth, kernel=kernel)
+
+
+    def _compute_outliers(self):
+
+        self.clf.fit(X)
+        self.log_dens = self.clf.score_samples(X)
+        outliers = [k for k,v in self.log_dens if v < self.threshold]
+    
+        return outliers
+
+    def _show_kd_plots(self):
+        outliers = self._compute_outliers()
+        
+        NC = len(self.component_combinations)
+        
+        cols, rows = 3, np.ceil(NC/3.0)
+        plt.figure(figsize=(15,5*rows))
+        
+        for i, combo in enumerate(self.component_combinations):
+            # Get feature pairs
+            X = self.X[:,combo]
+            
+            # Fit OC-SVM to data
+            self.clf.fit(X)
+            
+            # Get gridpoints for use in contour plots
+            xx, yy = np.meshgrid(np.linspace(X[:,0].min()-2, X[:,0].max()+2, 500), 
+                                 np.linspace(X[:,1].min()-2, X[:,1].max()+2, 500))
+
+            # Compute levels lines and points
+            Z = self.log_dens.reshape(X.shape)
+            
+            # Define plot location and set title
+            subplot = plt.subplot(rows,cols, i+1)
+            subplot.set_title(combo)
+            
+            # Plot contours
+            subplot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), self.threshold, 7),
+                             cmap=plt.cm.Blues_r)
+            
+            # Plot orange 'in-class'-region
+            subplot.contourf(xx, yy, Z, levels=[0, Z.max()],
+                             colors='orange')
+            
+            # Plot threshold boundary
+            a = subplot.contour(xx, yy, Z, levels=[self.threshold],
+                                linewidths=2, colors='red')
+            
+            # Plot margin region
+            subplot.contourf(xx, yy, Z, levels=[self.threshold,0],
+                             colors='blue', alpha=0.5)
+            
+            # Add points
+            b = subplot.scatter(X[:, 0], X[:, 1], c='white')
+            c = subplot.scatter(X[outliers, 0], X[outliers, 1], c='black')
+            
+            subplot.axis('tight')
+            subplot.legend(
+                [a.collections[0], b, c],
+                ['dec. func.', 'inliers', 'outliers'],
+                prop=matplotlib.font_manager.FontProperties(size=11))
+            
+            subplot.set_xlim((X[:,0].min()-2, X[:,0].max()+2))
+            subplot.set_ylim((X[:,1].min()-2, X[:,1].max()+2))
+        #plt.subplots_adjust(0.04, 0.1, 0.96, 0.94, 0.1, 0.26)
+
+        plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pareto_clustering/matlab/CalculateSimplexTratiosPCHA_mod.m
+++ pareto_clustering/matlab/CalculateSimplexTratiosPCHA_mod.m
@@ -1,10 +1,10 @@
-function ShufToOrigArcRatio = CalculateSimplexTratiosPCHA_mod(Xs, shuffInd)
+function ShufToOrigRatio = CalculateSimplexTratiosPCHA_mod(Xs, shuffInd)
 
 % Xs is the data subset
 % shuffInd is the index-array that governs the order of shuffling
 
 N = size(Xs,1); M = size(Xs,2);
-numArch = M + 1;
+%numArch = M + 1;
 
 % Make a shuffled version of X
 for i=1:M
@@ -12,17 +12,22 @@ for i=1:M
 end
 
 % PCHA algorithm init params
-delta = 0;
-I = 1:N; U = 1:N;
+%delta = 0;
+%I = 1:N; U = 1:N;
 
 % PCHA for shuffled and non-shuffled
-[Arch3Shuf,~,~,~,varexplShuf]=PCHA1(X_shuff',numArch,I,U,delta);
-[Arch3Orig,~,~,~,varexplOrig]=PCHA1(Xs',numArch,I,U,delta);
+%[Arch3Shuf,~,~,~,varexplShuf]=PCHA1(X_shuff',numArch,I,U,delta);
+%[Arch3Orig,~,~,~,varexplOrig]=PCHA1(Xs',numArch,I,U,delta);
 
 % Calculating the volumes of simplices
-ArchShufRed = bsxfun(@minus,Arch3Shuf,Arch3Shuf(:,numArch));
-VolArchShuf = abs(det(ArchShufRed(:,1:end-1))/factorial(M));
-ArchOrigRed = bsxfun(@minus,Arch3Orig,Arch3Orig(:,numArch));
-VolArchOrig = abs(det(ArchOrigRed(:,1:end-1))/factorial(M));
+%ArchShufRed = bsxfun(@minus,Arch3Shuf,Arch3Shuf(:,numArch));
+%VolArchShuf = abs(det(ArchShufRed(:,1:end-1))/factorial(M));
+%ArchOrigRed = bsxfun(@minus,Arch3Orig,Arch3Orig(:,numArch));
+%VolArchOrig = abs(det(ArchOrigRed(:,1:end-1))/factorial(M));
 
-ShufToOrigArcRatio =VolArchShuf./VolArchOrig
+%ShufToOrigArcRatio =VolArchShuf./VolArchOrig
+
+scatterplot(Xs)
+scatterplot(X_shuff)
+
+ShufToOrigRatio = ConvexHull(X_shuff) / ConvexHull(Xs);
