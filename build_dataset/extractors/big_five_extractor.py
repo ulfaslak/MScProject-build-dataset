ae9f0b4366f89cfa940ca94d8cb519d7640a46b5
ulfaslak <ulfjensen@gmail.com>
2016-03-27 11:56:18 +0300

    changed formatting of output from big_five_extractor to numpy array sorted like 'OCEAN'

--- build_dataset/extractors/big_five_extractor.py
+++ build_dataset/extractors/big_five_extractor.py
@@ -140,5 +140,5 @@ class Big_five_extractor:
         user_raw_value = loader.get_raw_value("user", user)
         bfi_values = self.dataset[user_raw_value]
         ocean = ['openness', 'conscientiousness', 'extraversion', 'aggreeableness', 'neuroticism']
-        return np.array([bfi_values[t] for k in ocean])
+        return np.array([bfi_values[t] for t in ocean])
 
